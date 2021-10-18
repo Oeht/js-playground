63c91059235e3c93a812d237318669dd5d2048fa
Theofanis Savvas <oeht.mobile@gmail.com>
2021-10-18 13:48:06 +0200

    os cpus

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const {sayhello, peter} = require('./names')
 const os = require('os')
 
-//const user = os.userInfo()
-//console.log(os.cpus())
+const user = os.userInfo()
+os.cpus().forEach(element => {
+    console.log(element.model);
+});
 
 console.log(sayhello(peter))
